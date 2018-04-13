e606d73e1352cdc55737b37674e85afa3e8456b7
Andy Nguyen <andrew.nguyen@colorado.edu>
2018-04-13 01:11:29 -0600

    redesign the answers metabox. rows instead of columns

--- resources/views/questions/answers-metaboxes.php
+++ resources/views/questions/answers-metaboxes.php
@@ -1,29 +1,18 @@
 <?php
 namespace ABetterBalance\Plugin;
-
 global $post;
 
 $questions = get_option( PaidSickTime::$questionsOptionName );
 $answers = Answers::getAnswers( $post->ID );
 ?>
 
-<div class="wrap">
-        <table id="repeatable-fieldset-one" width="100%">
-            <tbody>
-            <?php foreach ( (array) $questions as $k => $question ) : ?>
-                <tr>
-                    <td width="25%"><?php echo $question; ?></td>
-                    <td width="75%">
-                        <textarea placeholder="" rows="5" name="answers[]" style="width: 100%;"><?php echo !empty($answers[$k]) ? sanitize_textarea_field( $answers[$k] ) : ''; ?></textarea>
-                    </td>
-                </tr>
-            <?php endforeach; ?>
-            </tbody>
-        </table>
-
-
-        <p>
-            <?php wp_nonce_field( PaidSickTime::$nonce, PaidSickTime::$nonce ); ?>
-            <input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e('Save Changes') ?>" />
-        </p>
+<div class="question">
+<?php foreach ( (array) $questions as $k => $question ) : ?>
+    <p><?php echo nl2br($question); ?></p>
+    <textarea placeholder="" rows="5" name="answers[]" style="width: 100%;"><?php echo !empty($answers[$k]) ? sanitize_textarea_field( $answers[$k] ) : ''; ?></textarea>
+<?php endforeach; ?>
 </div>
+<p>
+    <?php wp_nonce_field( PaidSickTime::$nonce, PaidSickTime::$nonce ); ?>
+    <input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e('Save Changes') ?>" />
+</p>
