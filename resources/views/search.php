b062a0de81597511aa86282cf27987e0612ac041
Andy Nguyen <andrew.nguyen@colorado.edu>
2018-04-15 00:00:57 -0600

    update search page layout with columns

--- resources/views/search.php
+++ resources/views/search.php
@@ -4,6 +4,7 @@ global $post;
 
 $questions = get_option( PaidSickTime::$questionsOptionName );
 #$answers = Answers::getAnswers( $post->ID );
+$locations = []; #Locations:getLocations();
 ?>
 <h2>
 Overview of Paid Sick Time Laws in the United States:<br />
@@ -17,16 +18,39 @@ Custom Filtering
 
 
 
-<form name="pstl-search" method="post" action="" class="pstl-search">
-    <ul>
-    <?php foreach ( (array) $questions as $k => $question ) : ?>
-        <li class="question">
-            <label>
-                <input type="checkbox" name="questions[]" value="<?php echo $k; ?>" /><?php echo nl2br($question); ?>
-            </label>
-        </li>
-    <?php endforeach; ?>
-    </ul>
+<form name="pstl-search" method="post" action="" class="flex-row pstl-search">
+    <div class="col-1-2 issues-container">
+        <h3>Issues</h3>
+        <ul>
+            <?php foreach ( (array) $questions as $k => $question ) : ?>
+                <li class="question">
+                    <label>
+                        <input type="checkbox" name="questions[]" value="<?php echo $k; ?>" />
+                        <span><?php echo nl2br($question); ?></span>
+                    </label>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+    <div class="col-1-2 locations-container">
+        <h3>Locations</h3>
+
+        <div class="location-states">
+            <h4>States</h4>
+            <ul>
+                <?php foreach ( (array) $locations as $k => $location ) : ?>
+                    <li class="question">
+                        <label>
+                            <input type="checkbox" name="locations[]" value="<?php echo $k; ?>" />
+                            <span><?php echo nl2br($location); ?></span>
+                        </label>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+
+
+    </div>
     <p>
         <input type="submit" name="Submit" class="" value="<?php esc_attr_e('Submit') ?>" />
     </p>
