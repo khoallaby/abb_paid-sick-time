eeb8aeb4c6e71e33435447dd94b60b79d9a58bb8
Andy Nguyen <andrew.nguyen@colorado.edu>
2018-04-15 01:42:06 -0600

    integrate getPSTs() into search page

--- resources/views/search.php
+++ resources/views/search.php
@@ -5,11 +5,10 @@ global $post;
 $questions = get_option( PaidSickTime::$questionsOptionName );
 #$answers = Answers::getAnswers( $post->ID );
 $locations = []; #Locations:getLocations();
+$PSTs = PaidSickTime::getPSTs( true );
+
 ?>
-<h2>
-Overview of Paid Sick Time Laws in the United States:<br />
-Custom Filtering
-</h2>
+<h2>Overview of Paid Sick Time Laws in the United States:<br />Custom Filtering</h2>
 
 <p>To view a custom chart of data, please use the ﬁlter(s) below and click “Submit”.</p>
 
@@ -35,19 +34,27 @@ Custom Filtering
     <div class="col-1-2 locations-container">
         <h3>Locations</h3>
 
-        <div class="location-states">
-            <h4>States</h4>
-            <ul>
-                <?php foreach ( (array) $locations as $k => $location ) : ?>
-                    <li class="question">
+            <div class="location-states">
+                <?php if( isset($PSTs['state']) ) : ?>
+                <h4>States</h4>
+                <ul>
+                    <?php foreach ( $PSTs['state'] as $state ) : ?>
+                        <li class="location">
+                            <label>
+                                <input type="checkbox" name="locations[]" value="<?php echo $state->ID; ?>" />
+                                <span><?php echo $state->post_title; ?></span>
+                            </label>
+                        </li>
+                    <?php endforeach; ?>
+                    <li class="location">
                         <label>
-                            <input type="checkbox" name="locations[]" value="<?php echo $k; ?>" />
-                            <span><?php echo nl2br($location); ?></span>
+                            <input type="checkbox" class="location-all" />
+                            <span><em>All the above</em></span>
                         </label>
                     </li>
-                <?php endforeach; ?>
-            </ul>
-        </div>
+                </ul>
+                <?php endif; ?>
+            </div>
 
 
     </div>
