e39155abe0c0a8e9a777fd75ceac3daaf4874a27
Andy Nguyen <andrew.nguyen@colorado.edu>
2018-04-16 00:45:22 -0600

    format edit PST answers

--- resources/views/questions/answers-metaboxes.php
+++ resources/views/questions/answers-metaboxes.php
@@ -6,12 +6,12 @@ $questions = get_option( PaidSickTime::$questionsOptionName );
 $answers = Answers::getAnswers( $post->ID );
 ?>
 
-<div class="question">
 <?php foreach ( (array) $questions as $k => $question ) : ?>
-    <p><?php echo nl2br($question); ?></p>
+<div class="question" style="margin-bottom: 50px;">
+    <h3><?php echo nl2br($question); ?></h3>
     <textarea placeholder="" rows="5" name="answers[]" style="width: 100%;"><?php echo !empty($answers[$k]) ? sanitize_textarea_field( $answers[$k] ) : ''; ?></textarea>
-<?php endforeach; ?>
 </div>
+<?php endforeach; ?>
 <p>
     <?php wp_nonce_field( PaidSickTime::$nonce, PaidSickTime::$nonce ); ?>
     <input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e('Save Changes') ?>" />
